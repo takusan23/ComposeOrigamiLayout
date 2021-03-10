051ba68593510ef8c89a4551095d3ec69137a9a0
takusan23 <takumu1200@gmail.com>
2021-03-11 02:17:08 +0900

    なんか動かない

--- sample/src/main/java/io/github/takusan23/composeorigamilayoutsample/MainActivity.kt
+++ sample/src/main/java/io/github/takusan23/composeorigamilayoutsample/MainActivity.kt
@@ -3,8 +3,11 @@ package io.github.takusan23.composeorigamilayoutsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import io.github.takusan23.composeorigamilayout.OrigamiLayout
 import io.github.takusan23.composeorigamilayoutsample.ui.screen.HomeScreen
 import io.github.takusan23.composeorigamilayoutsample.ui.theme.ComposeOrigamiLayoutTheme
 
@@ -15,7 +18,21 @@ class MainActivity : ComponentActivity() {
             ComposeOrigamiLayoutTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    HomeScreen()
+                    // HomeScreen()
+                    OrigamiLayout {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Tag")
+                        }
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Tag")
+                        }
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Tag")
+                        }
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Tag")
+                        }
+                    }
                 }
             }
         }
